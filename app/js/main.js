ff2904be13a191f2f3cde812e0a038c098147608
karen9999 <karen.dev999@gmail.com>
2021-05-27 20:46:16 +0300

    add button click logic for gallery

--- app/js/main.js
+++ app/js/main.js
@@ -2,8 +2,8 @@ const openBtn = document.querySelector('.header__btn');
 const closeBtn = document.querySelector('.rightside-menu');
 
 openBtn.addEventListener('click', (e) => {
-  closeBtn.classList.remove('rightside-menu--close');
   e.stopPropagation();
+  closeBtn.classList.remove('rightside-menu--close');
 });
 
 document.addEventListener('click', () => {
@@ -14,4 +14,18 @@ $('.slider__inner').slick({
   dots: true,
   arrows: false,
   fade: true,
+  autoplay: true,
+  autoplaySpeed: 3000,
+});
+
+const galleryButtons = document.querySelectorAll('.gallery__btn');
+document.querySelector('.gallery__buttons').addEventListener('click', (e) => {
+  e.stopPropagation();
+  if (e.target.nodeName === 'DIV') {
+    return;
+  }
+  galleryButtons.forEach((btn) => {
+    btn.classList.remove('gallery__btn--active');
+  });
+  e.target.classList.add('gallery__btn--active');
 });
